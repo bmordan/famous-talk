da6634505b241d76c65dfa22f6c18433cfc9f8df
Bernard Mordan <bernardmordan@gmail.com>
2015-10-07 05:44:32 +0100

    sorts out particle positioning

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ var famous = require('famous')
 var FamousEngine = famous.core.FamousEngine
 var DOMElement = famous.domRenderables.DOMElement
 var Transitionable = famous.transitions.Transitionable
-
-var scene = FamousEngine.createScene()
+var Particle = famous.physics.Particle
+var Vec3 = famous.math.Vec3
+var PhysicsEngine = famous.physics.PhysicsEngine
+var Spring = famous.physics.Spring
 
 var someList = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven']
 
 FamousEngine.init()
+var physicsEngine = new PhysicsEngine()
+var scene = FamousEngine.createScene()
+var physicsTick = scene.addComponent({
+  onUpdate: function (time) {
+    physicsEngine.update(time)
+    scene.requestUpdateOnNextTick(physicsTick)
+  }
+})
+scene.requestUpdate(physicsTick)
 
 var listItemY = new Transitionable(0)
 
@@ -15,8 +26,8 @@ var ulNode = scene.addChild()
   .setSizeMode(0, 1, 1)
   .setProportionalSize(0.5, 0, 0)
   .setAbsoluteSize(0, 120, 0)
-  .setAlign(0.5, 0.51, 0)
-  .setOrigin(0.5, 0, 0)
+  .setAlign(0, 0.2, 0)
+  .setOrigin(0.5, 0.5, 0)
   .setMountPoint(0.5, 1, 0)
 new DOMElement(ulNode, {
   tagName: 'ul',
@@ -27,16 +38,23 @@ new DOMElement(ulNode, {
 
 
 
+var force = addGravity(physicsEngine)
+
+
 
 
 someList.forEach(function (item, i) {
+  var x = (window.innerWidth/100)*50
+  var y = ((window.innerHeight/100)*25) + (i*36) 
   var liNode = ulNode.addChild()
     .setSizeMode(0, 1, 0)
     .setProportionalSize(1, 0, 0)
     .setAbsoluteSize(0, 30)
     .setOrigin(0, 0)
     .setAlign(0, 0)
-  var li = new DOMElement(liNode, {
+    .setPosition(x, y, 0)
+  new DOMElement(liNode, {
+    id: 'li'+i,
     tagName: 'li',
     content: item,
     properties: {
@@ -44,8 +62,9 @@ someList.forEach(function (item, i) {
       'border-radius': '5px'
     }
   })
+  console.log(item,':',x, y)
+  new addPhysics(liNode, x, y)
 })
-
 
 
 
@@ -68,6 +87,65 @@ function positionListItems (curve) {
 
 
 
+function addGravity (physicsEngine) {
+  var gravity = new famous.physics.Gravity3D(null, physicsEngine.bodies, {
+    strength: -3e7,
+    max: 3000,
+    anchor: new Vec3(0, 0, 0)
+  })
+  physicsEngine.add(gravity)
+  return gravity  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function addPhysics (node, x, y) {
+  this.id = node.addComponent(this)
+  this.node = node
+  this.body = new Particle({
+    mass: 1,
+    position: new Vec3(x, y, 0)
+  })
+  this.force = new Spring(null, this.body, {
+    period: 1,
+    dampingRatio: 0.12,
+    anchor: new Vec3(x, y, 0)
+  })
+  physicsEngine.add(this.body, this.force)
+  node.requestUpdate(this.id)
+}
+addPhysics.prototype.onUpdate = function () {
+  var pos = this.body.position
+  this.node.setPosition(pos.x, pos.y, pos.z)
+  this.node.requestUpdateOnNextTick(this.id)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -80,15 +158,16 @@ function positionListItems (curve) {
 
 
 // Bernard's Helpers
-addTitle('Adding Elements')
+addTitle('Adding Physics')
 addControls()
+
 
 function addTitle (title) {
   var titleNode = scene.addChild()
     .setSizeMode(0, 1, 0)
     .setProportionalSize(0.5, 0, 0)
     .setAbsoluteSize(0, 120, 0)
-    .setAlign(0.5, 0.2, 0)
+    .setAlign(0.5, 0.1, 0)
     .setOrigin(0.5, 0.5, 0)
     .setMountPoint(0.5, 0.5, 0)
   var linkNode = titleNode.addChild()
@@ -140,7 +219,16 @@ function addControls () {
     .setPosition(90, 0, 0)
   new DOMElement(animationNode, {tagName: 'input'})
     .setAttribute('name', 'ease')
-    .setAttribute('placeholder', 'curve') 
+    .setAttribute('placeholder', 'curve')
+  var mouseNode = controlNode.addChild()
+    .setSizeMode(1, 1)
+    .setAbsoluteSize(60, 60)
+    .setPosition(190, 0)
+  new DOMElement(mouseNode, {
+    id: 'mouseOn',
+    tagName: 'input'
+  }).setAttribute('type', 'checkbox').setAttribute('value', 'mouse')
+  addMouseEvents(mouseNode)
   addAlignEvents(inputNode)
   addAnimationEvents(animationNode)
 }
@@ -154,7 +242,7 @@ function addAlignEvents (node) {
         return parseInt(v)
       })
       console.log(values)
-      ulNode.setAlign(values[0], values[1], 0)
+      force.anchor.set(values[0], values[1], 0)
     }
   }
 }
@@ -167,6 +255,20 @@ function addAnimationEvents (node) {
       positionListItems(curve)
     }
   }
+}
+function addMouseEvents (node) {
+  node.addUIEvent('click')
+  node.onReceive = function (event, payload) {
+    var mouseOn = document.getElementById('mouseOn').checked
+    if (mouseOn) {
+      document.addEventListener('mousemove', mouseGravity)
+    } else {
+      document.removeEventListener('mousemove', mouseGravity)
+    }
+  }
+}
+function mouseGravity (e) {
+  force.anchor.set(e.pageX, e.pageY)
 }
 function resetList () {
   return ulNode.getChildren().map(function (li) {
